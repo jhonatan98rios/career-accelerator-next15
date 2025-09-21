5ad6a7825d778fc5f381ab62069a88a536802c7e
Jhonatan Teixeira Rios <jhonatan98rios@gmail.com>
2025-09-20 21:29:14 -0300

    implementing auth in the register page

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -6,6 +6,7 @@ import { createSubscription } from '@/lib/subscription';
 import { Profile } from '@/models/Profile';
 import { HttpStatus } from '@/types/httpStatus';
 import { log, LogLevel } from "@/lib/logger";
+import { isAuthenticated } from '@/lib/auth0';
 
 type RegisterBody = {
   name: string;
@@ -17,56 +18,69 @@ type RegisterBody = {
 
 export async function POST(req: Request) {
 
-  // TODO: All this information are available into the JWT token
-  const body = await req.json();
-  const { name, email, plan, sub, picture }: RegisterBody = body;
+  try {
+    const token = await isAuthenticated(req.headers)
+  
+    const body = await req.json();
+    const { name, email, plan, picture, sub }: RegisterBody = body;
 
-  if (!(name && email && plan && sub && picture)) {
-    await log(LogLevel.ERROR, "Missing required registration fields", { name, email, plan, sub, picture });
-    return NextResponse.json({ error: 'Missing required registration fields' }, { status: HttpStatus.BAD_REQUEST });
-  }
+    if (sub != token.sub) {
+      await log(LogLevel.ERROR, "POST /register: Failed to authenticate the user", { name, email, plan, sub, picture });
+      return NextResponse.json({ error: 'Failed to authenticate the user' }, { status: HttpStatus.UNAUTHORIZED });
+    }
+  
+    if (!(name && email && plan && sub && picture)) {
+      await log(LogLevel.ERROR, "POST /register: Missing required registration fields", { name, email, plan, sub, picture });
+      return NextResponse.json({ error: 'Missing required registration fields' }, { status: HttpStatus.BAD_REQUEST });
+    }
+  
+    await log(LogLevel.INFO, "POST /register: Registering user", { email, plan });
 
-  await log(LogLevel.INFO, "Registering user", { email, plan });
-
-  await connectDB();
-
-  const existing = await Profile.findOne({ email });
-  if (existing) {
-    await log(LogLevel.WARN, "User already exists", { email });
-    return NextResponse.json(null, { status: HttpStatus.CONFLICT });
-  }
-
-  if (!Object.values(Plan).includes(plan)) {
-    await log(LogLevel.ERROR, "Invalid plan selected", { email, plan });
-    return NextResponse.json({ error: 'Invalid plan selected' }, { status: HttpStatus.BAD_REQUEST });
-  }
-
-  await log(LogLevel.INFO, "Creating subscription", { email, plan });
-  const subscription = await createSubscription({
-    plan,
-    email,
-  })
-
-  // Create the user and send the email in parallel
-  await log(LogLevel.INFO, "Creating a new user", { email, plan, subscriptionId: subscription.subscription_id });
-
-  const promises = [
-    Profile.create({
-      name,
-      email,
+    await connectDB();
+  
+    const existing = await Profile.findOne({ email });
+    if (existing) {
+      await log(LogLevel.WARN, "POST /register: User already exists", { email });
+      return NextResponse.json(null, { status: HttpStatus.CONFLICT });
+    }
+  
+    if (!Object.values(Plan).includes(plan)) {
+      await log(LogLevel.ERROR, "POST /register: Invalid plan selected", { email, plan });
+      return NextResponse.json({ error: 'Invalid plan selected' }, { status: HttpStatus.BAD_REQUEST });
+    }
+  
+    await log(LogLevel.INFO, "POST /register: Creating subscription", { email, plan });
+    const subscription = await createSubscription({
       plan,
-      picture,
-      externalAuthId: sub,
-      status: UserStatus.INACTIVE,
-    }),
-    sendPaymentEmail({ name, to: email, plan, paymentLink: subscription.init_point }),
-  ]
+      email,
+    })
+  
+    // Create the user and send the email in parallel
+    await log(LogLevel.INFO, "Creating a new user", { email, plan, subscriptionId: subscription.subscription_id });
+  
+    const promises = [
+      Profile.create({
+        name,
+        email,
+        plan,
+        picture,
+        externalAuthId: sub,
+        status: UserStatus.INACTIVE,
+      }),
+      sendPaymentEmail({ name, to: email, plan, paymentLink: subscription.init_point }),
+    ]
+  
+    await Promise.all(promises)
+    .catch(async (error) => {
+      await log(LogLevel.ERROR, "POST /register: Error during user registration", { error, email, plan });
+      return NextResponse.json({ error: 'Failed to create user and subscription' }, { status: HttpStatus.INTERNAL_SERVER_ERROR });
+    })
+  
+    return NextResponse.json(null, { status: HttpStatus.CREATED });
+  }
+  catch (err: any) {
+    await log(LogLevel.ERROR, "POST /register: Exception occurred", { error: err });
+    return NextResponse.json({ error: err.message || "Internal Server Error" }, { status: HttpStatus.INTERNAL_SERVER_ERROR });
 
-  await Promise.all(promises)
-  .catch(async (error) => {
-    await log(LogLevel.ERROR, "Error during user registration", { error, email, plan });
-    return NextResponse.json({ error: 'Failed to create user and subscription' }, { status: HttpStatus.INTERNAL_SERVER_ERROR });
-  })
-
-  return NextResponse.json(null, { status: HttpStatus.CREATED });
+  }
 }
